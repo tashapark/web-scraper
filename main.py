f03bd2a446bc9bb326fbe0e9051f5d8cf1df10d0
TashaPark <pdy2564@naver.com>
2024-04-18 18:21:39 +0900

    import module, while

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
-# input은 하나의 argument만 받음. 유저가 답변을 해야 끝남. 엔터쳐야 함. 
-# 뭐든지 치면 return에 저장 
-# input은 이미 있는 built-in function
+"""
+built in이 아니면 python standard library에서 module을 import 해야 함
+같은 모듈에서 여러 개는 ,로 가지고 오면 됨. 아래는 function만 쓰면 됨. 
+ 여러 줄을 주석 할 때는 "*3를 쓰면 됨. 앞 뒤에 
+"""
+# casino
+from random import randint
 
-#비교를 위한 숫자로 변환
-age = int(input("How old are you?"))
+print("Welcome to Python Casino")
+pc_choice = randint(1, 100)
 
-print("user answer", age)
 
-# 형확인.
-print(type(age))
+playing = True
 
-# drink age calculator
-if age < 18: 
-    print("You can't drink.")
-elif 18 <= age and age <= 35 : 
-    print("You can drink beer!")
-elif age == 60 or age == 70:
-    print("Birthday party!")
-elif age == 19: 
-    print("You are an adult! have fun!") 
-else:
-    print("Go ahead!")
+while playing:
+    user_choice = int(input("Choose number (1-100):"))
+    if user_choice == pc_choice:
+        print("You won!!")
+        playing = False
+    elif user_choice > pc_choice:
+        print("Lower!")
+    elif user_choice < pc_choice:
+        print("Higher!")
+
+
+# while은 control flow로 if와 같지만 멈추지 않음. if는 1번만 실행. 무조건 멈출 수 있는 값을 적용해야 함.
+
+distance = 0
+
+while distance < 20:
+    print("I'm running:", distance, "km")
+    # 0-19까지 해서 멈춤. 
+    distance = distance + 1 
+
+
