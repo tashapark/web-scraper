b61a95d131eb9d22e6cddea9b2fcb48e59d6c264
TashaPark <pdy2564@naver.com>
2024-04-18 17:41:14 +0900

    age calculator

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
-# 조건문
+# input은 하나의 argument만 받음. 유저가 답변을 해야 끝남. 엔터쳐야 함. 
+# 뭐든지 치면 return에 저장 
+# input은 이미 있는 built-in function
 
-password_correct = False
+#비교를 위한 숫자로 변환
+age = int(input("How old are you?"))
 
-if password_correct:
-    print("Here is your money")
-# else는 필요하면 써도 되고 아니면 안 써도 됨
+print("user answer", age)
+
+# 형확인.
+print(type(age))
+
+# drink age calculator
+if age < 18: 
+    print("You can't drink.")
+elif 18 <= age and age <= 35 : 
+    print("You can drink beer!")
+elif age == 60 or age == 70:
+    print("Birthday party!")
+elif age == 19: 
+    print("You are an adult! have fun!") 
 else:
-    print("Wrong password")
-
-
-winner = 8
-
-if winner > 10:
-    print("Winner is greater than 10")
-# elif 필요하면 계속 추가해도 됨. 
-elif winner <10:
-    print("Winner is less than 10")
-else: 
-    print("Winner is 10")
+    print("Go ahead!")
