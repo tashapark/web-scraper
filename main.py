c75ff153156931b8a37015c1d1d48b0b88039c30
TashaPark <pdy2564@naver.com>
2024-04-23 01:02:36 +0900

    to understand class with self, prameter, argument

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 class Puppy:
 # method는 펑션 안에 있는 class
 # class 안에 method를 가질 경우, 첫 번째 argument는 자동으로 self임.
-    def __init__(self):
-        self.name = "Ruffus"
+# self 이외에 계속 업데이트 될 변수를 파라미터로 넣어줌. 
+    def __init__(self, name, breed):
+        self.name = name
         self.age = 0.1
-        self.breed = "Beagle"
+        self.breed = breed
 
 # self가 곧 루푸스임. 첫번째 argument라서 
 # but, 밑에다가 bibi = Puppy()를 만들어도 출력은 계속 ruffus로 나옴
-ruffus = Puppy()
+# 위에서 넣어준 파라미터 값을 함수에 추가 해주면 됨. 
+# 위치 argument 사용 중임. 
+ruffus = Puppy(
+    "Ruffus", 
+    "Beagle"
+    )
+bibi = Puppy(
+    "Bibi", 
+    "Dalmatian"
+    )
 
-print(ruffus.name, ruffus.age, ruffus.breed)
+print(ruffus.name, bibi.name)
