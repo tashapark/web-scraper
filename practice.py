ae013b932b33e942f9041bf156c9bd5867ca5ea8
TashaPark <pdy2564@naver.com>
2024-05-03 22:08:30 +0900

    :poop::construction:

--- practice.py
+++ practice.py
@@ -1,21 +1,15 @@
-import random
-
-ANSWER = random.randint(1, 20)
-num_tries = 4
-
-while 0 < num_tries:
-    user = int(input(f"기회가 {num_tries}번 남았습니다. 1-20 사이의 숫자를 맞혀 보세요:"))
-    if ANSWER == user:
-      print(f"축하합니다. {num_tries}번 만에 숫자를 맞히셨습니다.")
-      break
-    elif ANSWER != user:
-       if ANSWER > user:
-          print("Up")
-       elif ANSWER < user:
-          print("Down")
-    num_tries -=1
-
-if num_tries ==0 :
-        print(f"아쉽습니다. 정답은 {ANSWER}입니다.")
 
 
+
+with open(vocabulary.txt, 'a') as f:
+   
+    file = open("vocabulary.txt", "w")
+    writing = True
+
+    while writing:
+        eng_word =  f.write(input("영어 단어를 입력하세요:"))
+        kor_word = input("한국어 뜻을 입력하세요:")
+        if eng_word or kor_word == "q":
+            writing = False
+            print("Check out the vocabulary.txt")
+        
