c8c7b93ce2a5050c4004e191238cd719711d4370
TashaPark <pdy2564@naver.com>
2024-05-06 00:18:14 +0900

    :bug:random words with dicts and lists

--- practice.py
+++ practice.py
@@ -3,28 +3,33 @@ import random
 
 voca_dicts ={}
 
+with open("vocabulary.txt", 'r') as f:   
+    for line in f:
+        data = line.strip().split(": ")
+        if len(data) < 2:
+            continue
+        eng_word, kor_word = data[0], data[1]
+        voca_dicts[eng_word] = kor_word
+
+
 writing = True 
 while writing: 
-    with open("vocabulary.txt", 'r') as f:   
-        for line in f:
-            data = line.strip().split(": ")
-            if len(data) < 2:
-                continue
-            eng_word, kor_word = data[0], data[1]
-            voca_dicts[eng_word] = kor_word
+        # 섞을 수 있게 리스트로 
+        voca_keys = list(voca_dicts.keys())
+        random.shuffle(voca_keys)
+        # 이것도 방법임. 인덱스에 랜덤 수 할당. 
+        # index = random.randint(0, len(voca_keys) - 1)
+        # english_word = voca_keys[index]
 
-            # 섞을 수 있게 리스트로 
-            voca_keys = list(voca_dicts.keys())
-            random.shuffle(voca_keys)
-           
-            for eng_word in voca_keys:
-                kor_word = voca_dicts[eng_word]
-                answer = input(f"{kor_word}: ")
-                if eng_word == answer:
-                    print("맞았습니다!") 
-                else: 
-                    print(f"아쉽습니다. 정답은 {eng_word}입니다.")
-    
+        for eng_word in voca_keys:
+            kor_word = voca_dicts[eng_word]
+            answer = input(f"{kor_word}: ")
             if answer == "q":
                 writing = False
                 break
+            elif eng_word == answer:
+                print("맞았습니다!") 
+            else: 
+                print(f"아쉽습니다. 정답은 {eng_word}입니다.")
+    
+           
