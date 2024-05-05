e25b968513a58361e067c0bb881faa19a50c2529
TashaPark <pdy2564@naver.com>
2024-05-06 00:02:55 +0900

    random words with dicts and lists

--- practice.py
+++ practice.py
@@ -1,19 +1,30 @@
-
    
+import random
+
+voca_dicts ={}
 
 writing = True 
 while writing: 
     with open("vocabulary.txt", 'r') as f:   
         for line in f:
-            data = line.strip().split(": ") 
+            data = line.strip().split(": ")
             if len(data) < 2:
-                continue  # 올바른 형식의 단어 쌍이 아닌 경우 건너뜀..
-            # 파일에는 제대로 저장되었지만, 입력중에 띄어쓰기가 된 것들에서 에러가 나는 것으로 여겨짐. 
-            # 결국 모든 data에서 넘김 없이 출력했지만, 추가해서 단어 쌍으로 2개가 아닌 줄은 넘기도록 짜는게 추후에도 좋을 듯. 
+                continue
             eng_word, kor_word = data[0], data[1]
-            answer = input(f"{kor_word}: ")
-            if eng_word == answer:
-                print("맞았습니다!") 
-            else: 
-                print(f"아쉽습니다. 정답은 {eng_word}입니다.")
-    writing = False
+            voca_dicts[eng_word] = kor_word
+
+            # 섞을 수 있게 리스트로 
+            voca_keys = list(voca_dicts.keys())
+            random.shuffle(voca_keys)
+           
+            for eng_word in voca_keys:
+                kor_word = voca_dicts[eng_word]
+                answer = input(f"{kor_word}: ")
+                if eng_word == answer:
+                    print("맞았습니다!") 
+                else: 
+                    print(f"아쉽습니다. 정답은 {eng_word}입니다.")
+    
+            if answer == "q":
+                writing = False
+                break
