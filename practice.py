e893e92a5bae5cef172a2512d6ddbc54d9182a55
TashaPark <pdy2564@naver.com>
2024-05-05 21:47:12 +0900

    input contents save in txt file

--- practice.py
+++ practice.py
@@ -4,8 +4,8 @@
 with open("vocabulary.txt", 'a') as f:   
     writing = True 
     while writing:
-        eng_word =  input("영어 단어를 입력하세요:")
-        kor_word =  input("한국어 뜻을 입력하세요:")
+        eng_word =  input("영어 단어를 입력하세요: ")
+        kor_word =  input("한국어 뜻을 입력하세요: ")
 
         if eng_word == "q" or kor_word == "q":
             writing = False
