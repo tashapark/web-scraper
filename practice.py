ea2ade43e73074023f221ac77f021785e4a69ea3
TashaPark <pdy2564@naver.com>
2024-05-14 17:14:43 +0900

    lottery) check nums

--- practice.py
+++ practice.py
@@ -36,14 +36,48 @@ def draw_winning_numbers():
 
 # 유저 입력 값과 매치되는 지 확인하기 
 def count_matching_numbers(numbers, winning_numbers):
-    # 여기에 코드를 작성하세요
     count = 0 
     for num in numbers:
         if num in winning_numbers:
             count += 1
     return count        
     
+# 번호 확인해서 당첨금 제시하기 
+def check(numbers, winning_numbers):
+    # 여기에 코드를 작성하세요
+    if count_matching_numbers(numbers, winning_numbers) == 6:
+        if numbers[0:] == winning_numbers[:6]:
+            return 10000000000
+        else:
+            return 500000000
+    elif count_matching_numbers(numbers, winning_numbers) == 5:
+        return 1000000
+    elif count_matching_numbers(numbers, winning_numbers) == 4:
+        return 50000
+    elif count_matching_numbers(numbers, winning_numbers) == 3:
+        return 5000
+    else:
+        print("take a rain check :(")
+        
+        
+# 이렇게 변수명 지정하면 중복 길이를 줄일 수 있음. 
+# def check(numbers, winning_numbers):
+#     count = count_matching_numbers(numbers, winning_numbers[:6])
+#     bonus_count = count_matching_numbers(numbers, winning_numbers[6:])
+
+#     if count == 6:
+#         return 1000000000
+#     elif count == 5 and bonus_count == 1:
+#         return 50000000
+#     elif count == 5:
+#         return 1000000
+#     elif count == 4:
+#         return 50000
+#     elif count == 3:
+#         return 5000
+#     else:
+#         return 0
 
 # 테스트 코드
-print(count_matching_numbers([2, 7, 11, 14, 25, 40], [2, 11, 13, 14, 30, 35]))
-print(count_matching_numbers([2, 7, 11, 14, 25, 40], [14]))
+print(check([2, 4, 11, 14, 25, 40], [4, 12, 14, 28, 40, 41, 6]))
+print(check([2, 4, 11, 14, 25, 40], [2, 4, 10, 11, 14, 40, 25]))
